58e3348260631a52a37dba2c9a963f3da0429319
Moaz Atef <moazsaady6@gmail.com>
2023-05-08 18:46:43 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,338 @@
+#include <graphics.h>
+#include <climits>
+#include <queue>
+
+
+using namespace std;
+
+//Function for every element in this game
+const int CELL_SIZE = 18;
+const int EMPTY = 0;
+const int SNAKEBODY = 1;
+const int APPLE = 2;
+const int WALL = 3;
+enum  dirctions{UP,DOWN,LEFT,RIGHT};
+//the current dirc of snake
+dirctions snakedirction = RIGHT;
+bool isgameOver = false;
+
+struct Location{
+                        int row;
+                        int col;
+} ;
+
+queue < Location > snakeboody;
+
+
+//this array draw a game its array is to facilitate the drawing of the game
+int  board [60][60] =
+    {   {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3},
+        {3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3}
+
+
+};
+
+
+//this function to draw wall in every side from GUI App
+void drawwall(int row, int col)
+{
+    int left = col*CELL_SIZE;
+    int top  = row*CELL_SIZE;
+    int right= left+CELL_SIZE;
+    int bottom= top+CELL_SIZE;
+
+    //style of blocks to draw
+    setfillstyle(SOLID_FILL,RED);
+    bar(left,top,right,bottom);
+
+    //to solid block to black color
+    setcolor(BLACK);
+    setlinestyle(SOLID_LINE,2,NORM_WIDTH);
+
+    //to draw blocks in for loop
+    line(left+0  , top+9 , left+18 ,top+9);
+    line(left+12 , top+0 , left+12 ,top+9);
+    line(left+4  , top+9 , left+4  ,bottom);
+    line(left    , bottom, right   ,bottom);
+    line(left    , top   , right   ,top  );
+
+}
+
+//to draw a snake body
+
+void erasesnakebody(int row , int col )
+{
+    int left= col*CELL_SIZE;
+    int top=  row*CELL_SIZE;
+    //style of snake
+    setfillstyle(SOLID_FILL, BLACK);
+    bar(left , top , left+16 , top+16 );
+}
+void drawsnakebody(int row , int col )
+{
+    int left= col*CELL_SIZE;
+    int top=  row*CELL_SIZE;
+    //style of snake
+    setfillstyle(SOLID_FILL,YELLOW);
+    bar(left+1 , top+1 , left+16 , top+16 );
+}
+
+//to draw apple
+void drawapple(int row , int col )
+{
+
+    int left = col*CELL_SIZE;
+    int top  = row*CELL_SIZE;
+    // i have apple image and choise this img in this function
+    readimagefile("apple22.ico", left , top , left+15 , top+15);
+
+    }
+
+void drawboard()
+{
+for(int row=0; row<60; row++ )
+    {
+    for(int col=0; col<60;col++)
+    {
+
+    switch (board[row][col])
+{
+case  SNAKEBODY:
+
+    drawsnakebody( row , col);
+    break;
+
+
+case APPLE:
+
+    drawapple(row , col);
+    break;
+
+case WALL:
+
+    drawwall(row ,col);
+    break;
+}
+  }
+ }
+}
+//intiliaize the snake
+void intsnake(){
+    Location loc;
+        loc.row=2;
+        loc.col=10;
+
+         for(int i=0; i<=4; i++)
+      {
+         snakeboody.push(loc);
+         board[loc.row][loc.col] =1;
+         loc.row +=1;
+}
+}
+Location getnexthead(){
+
+     Location head =  snakeboody.back();
+    switch(snakedirction){
+    case UP:
+        head.row -=1; break;
+    case DOWN:
+        head.row +=1; break;
+    case LEFT:
+        head.col -=1; break;
+    case RIGHT:
+        head.col +=1; break;
+}
+  return head;
+
+}
+void generatenextapple(){
+
+     int row,col;
+     do{
+          row = rand() % 60;
+          col = rand() % 60;
+
+       }while(board[row][col] != EMPTY);
+
+       board[row][col] = APPLE;
+       drawapple(row,col);
+}
+void movesnaketonextlocation(Location nexthead)
+{
+    snakeboody.push(nexthead);
+    drawsnakebody(nexthead.row,nexthead.col);
+    board[nexthead.row][nexthead.col] = 1;
+
+    Location tail = snakeboody.front();
+    snakeboody.pop();
+    erasesnakebody(tail.row,tail.col);
+    board[tail.row][tail.col] = 0;
+
+}
+void moveandgrowsanke(Location nexthead)
+{
+    snakeboody.push(nexthead);
+    drawsnakebody(nexthead.row,nexthead.col);
+    board[nexthead.row][nexthead.col] = 1;
+
+}
+
+
+
+void erase()
+{
+    setfillstyle(SOLID_FILL , BLACK);
+    bar(0,0,1080,1080);
+}
+
+void gameover(){
+
+            isgameOver=true;
+            erase();
+            setcolor(WHITE);
+            settextstyle(10,4,6);
+            settextjustify(CENTER_TEXT,CENTER_TEXT);
+            outtextxy(getmaxx() / 2,getmaxy() / 2,"Game Over Man ...!!");
+            string sound1 = "gameover.wav";
+            PlaySound(sound1.c_str(),NULL,SND_ASYNC);
+
+}
+
+
+
+void movesnake(){
+    string sound = "bravoo3aleek.wav";
+    Location nexthead =  getnexthead();
+    //test my board
+    switch(board[nexthead.row][nexthead.col]){
+     case EMPTY:
+        movesnaketonextlocation(nexthead);
+        break;
+     case APPLE:
+         PlaySound(sound.c_str(),NULL,SND_ASYNC);
+        moveandgrowsanke(nexthead);
+        generatenextapple();
+        break;
+
+     case WALL:
+     case SNAKEBODY:
+        gameover();
+        break;
+    }
+
+}
+
+void changesnakedirection(char ch)
+{
+    switch (ch){
+        case KEY_UP: snakedirction=UP;
+        break;
+        case KEY_DOWN : snakedirction=DOWN;
+        break;
+        case KEY_RIGHT: snakedirction=RIGHT;
+        break;
+        case KEY_LEFT: snakedirction=LEFT;
+        break;
+    }
+ }
+void checkkeyinput()
+{
+    char ch ;
+    if (kbhit()){
+    ch = getch();
+    if (ch ==0 ){
+        ch = getch();
+          changesnakedirection(ch);
+
+    }
+    }
+
+}
+
+// function to draw text  on screen
+void drawText(int x, int y , const char* displayedText){
+
+outtextxy(x,y,displayedText);
+}
+
+int main(){
+              isgameOver=false;
+
+    const char* firstName = "press any key to start game";
+    const char* name0 = "Created By  :";
+    const char* name1 = "Moaz Atef";
+    const char* name2 = "Yahya Ehab";
+    const char* name3 = "Mostafa El shahaat";
+
+
+
+    initwindow(60 * CELL_SIZE, 60 * CELL_SIZE,"Snake game By Moaz");
+    settextstyle(3,0,4);
+    drawText(250,250,firstName);
+    settextstyle(4,0,3);
+    drawText(20,50,name0);
+    drawText(100,100,name1);
+    drawText(110,150,name2);
+    drawText(120,190,name3);
+
+
+    getch();
+    cleardevice();
+    intsnake();
+    drawboard();
+    //this loop for game for ever
+    int gametimer = 0;
+    while(! isgameOver)
+    {
+        if(gametimer == INT_MAX )
+            gametimer=0;
+        if(gametimer % 5500000 == 0)
+            movesnake();
+         gametimer++;
+         checkkeyinput();
+    }
+
+    while(! kbhit());
+    closegraph();
+
+ }
